f348c2dcbfb91df7fb99fb513a96a7889007e254
avery <avry@pm.me>
2022-12-05 01:39:16 -0800

    day 3 question 2

--- day3/q2.rs
+++ day3/q2.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, prelude::*, Result};
+use std::io::{BufReader, prelude::*};
 use std::fs::File;
 
 use std::collections::HashSet;
@@ -11,36 +11,36 @@ fn main() {
     let mut res: u32 = 0;
 
     loop {
-        let stream = reader.next().unwrap();
-        if stream.is_err() {
+        let stream = reader.next();
+        if stream.is_none() {
             break;
         }
-        let first = stream.unwrap();
+        let first = stream.unwrap().unwrap();
 
         let mut first_set = HashSet::new();
-        for char in first.chars() {
-            first_set.insert(char);
+        for chr in first.chars() {
+            first_set.insert(chr);
         }
 
         let second = reader.next().unwrap().unwrap();
         let mut second_set = HashSet::new();
-        for char in second.chars() {
-            second_set.insert(char);
+        for chr in second.chars() {
+            second_set.insert(chr);
         }
 
         let third = reader.next().unwrap().unwrap();
         let mut third_set = HashSet::new();
-        for char in third.chars() {
-            third_set.insert(char);
+        for chr in third.chars() {
+            third_set.insert(chr);
         }
 
-        let mut first_intersection = HashSet::new();
-        for char in first_set.intersection(&second_set) {
-            first_intersection.insert(char);
+        let mut first_intersection = HashSet::<char>::new();
+        for chr in first_set.intersection(&second_set) {
+            first_intersection.insert(*chr);
         }
 
         let badge = *first_intersection.intersection(&third_set).nth(0).unwrap();
-        let mut value = *badge as u32;
+        let mut value = badge as u32;
         if badge.is_ascii_uppercase() {
             value -= 38;
         }
