e38f6a9f5bf992ad8b402d55b509a1ad23dd2433
avery <avrypromo@pm.me>
2022-12-18 17:47:33 -0800

    to desktop

--- day7/q1.rs
+++ day7/q1.rs
@@ -1,17 +1,36 @@
 use std::io::{BufReader, prelude::*};
 use std::fs::File;
 
+use std::collections::HashMap;
+
+
+#[derive(Clone)]
+struct Dir {
+    size: u32,
+    children: Vec<String>,
+}
+
+impl Dir {
+    fn new() -> Self {
+        Dir { size: 0u32, children: Vec::<String>::new() }
+    }
+}
+
 
 fn main() {
     let f = File::open("input").unwrap();
     let mut reader = BufReader::new(f).lines();
 
+    let mut size = 0u32;
     let mut totals = 0u32;
-    let mut wd = Vec::<&str>::new();
+    let mut dirs = HashMap::<String, Dir>::new();
+
+    // current directory and working directory
+    let mut cd = Dir::new();
+    let mut wd = String::from("");
 
         while let Some(Ok(line)) = reader.next() {
             let mut split = line.split(' ');
-            let mut size = 0u32;
             let head = split.next().unwrap();
 
             match head {
@@ -19,19 +38,34 @@ fn main() {
                     if split.next().unwrap() == "cd" {
                         let dir = split.next().unwrap();
                         if dir == ".." {
-                            wd.pop();
+                            wd = wd.rsplitn(3, "/").nth(2).unwrap().to_string();
+                            cd = dirs.get_mut(&wd).unwrap();
                         } else {
-                            wd.push(dir);
+                            wd.push_str(dir);
+                            wd.push_str("/");
+                            if dirs.contains_key(&wd) {
+                                cd = dirs.get_mut(&wd).unwrap();
+                            } else {
+                                cd = Dir::new();
+                            }
+                            dirs.insert(wd.clone(), cd.to_owned());
                         }
-                        totals += size;
+                        // println!("{:?}", wd);
+                        if size >= 100000 {
+                            totals += size;
+                        }
                         size = 0;
                     }
                 }
                 "dir" => {
-                    continue;
+                    let kid = split.next().unwrap();
+                    let mut parent = wd.clone();
+                    parent.push_str(kid);
+                    cd.children.push(parent.to_owned());
                 }
                 _ => {
-                    size += u32::from_str_radix(head, 10).unwrap();
+                    cd.size += u32::from_str_radix(head, 10).unwrap();
+                    // println!("{size}");
                 }
             }
         }
